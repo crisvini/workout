a00bb35f3c9a7d0deadc7f183fdd713a58d4543a
Cristian Leoncini <crisvini.leoncini@outlook.com>
2022-05-08 19:49:16 -0300

    Adição de função que valida o cpf

--- js/functions.js
+++ js/functions.js
@@ -33,3 +33,31 @@ function alertaPreenchimento(idCampo, idLabel) {
         $(idCampo).removeClass("alerta-input");
     }
 }
+
+// Valida cpf
+function testaCpf(documento) {
+    if (documento.length == 11) {
+        var Soma;
+        var Resto;
+        Soma = 0;
+        // Elimina CPF invalidos conhecidos
+        if (documento == "00000000000" || documento == "11111111111" || documento == "22222222222" || documento == "33333333333" || documento == "44444444444" || documento == "55555555555" || documento == "66666666666" || documento == "77777777777" || documento == "88888888888" || documento == "99999999999")
+            return false;
+
+        for (i = 1; i <= 9; i++) Soma = Soma + parseInt(documento.substring(i - 1, i)) * (11 - i);
+        Resto = (Soma * 10) % 11;
+
+        if ((Resto == 10) || (Resto == 11)) Resto = 0;
+        if (Resto != parseInt(documento.substring(9, 10))) return false;
+
+        Soma = 0;
+        for (i = 1; i <= 10; i++) Soma = Soma + parseInt(documento.substring(i - 1, i)) * (12 - i);
+        Resto = (Soma * 10) % 11;
+
+        if ((Resto == 10) || (Resto == 11)) Resto = 0;
+        if (Resto != parseInt(documento.substring(10, 11))) return false;
+        return true;
+    } else {
+        return false;
+    }
+}
