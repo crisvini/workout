f581721195143600a2f32841ab8e4bbce8a57908
Cristian <crisvini.leoncini@outlook.com>
2022-08-02 08:55:39 -0300

    início cadastroUsuario.php

--- ajax/cadastroUsuario.php
+++ ajax/cadastroUsuario.php
@@ -1,2 +1,16 @@
 <?php
 
+include("./mysql/conexao.php");
+
+$sql =
+    "INSERT INTO 
+        usuarios (nome, email, cpf, nascimento, senha) 
+    VALUES 
+        ('" . $_POST['nome'] . "', '" . $_POST['email'] . "', '" . $_POST['cpf'] . "', '" . $_POST['nascimento'] . "', MD5('" . $_POST['senha'] . "'))";
+
+if ($conn->query($sql) === TRUE)
+    echo 'OK';
+else
+    echo 'erro';
+
+$conn->close();
