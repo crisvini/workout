73dbe3060edd3f948d421c8d7dc3d709f5c31cfe
Cristian <crisvini.leoncini@outlook.com>
2022-08-06 23:01:14 -0300

    adição de ultima ficha completa no banco

--- query/cadastroUsuario.php
+++ query/cadastroUsuario.php
@@ -13,13 +13,20 @@ if ($_SESSION['objetivo'] == "emagrecimento")
 else if ($_SESSION['objetivo'] == "hipertrofia")
     $_id_ultima_ficha = 4;
 
+// Define a última ficha completa
+$ultima_ficha_completa = 0;
+if ($_SESSION['objetivo'] == "emagrecimento")
+    $ultima_ficha_completa = 3;
+else if ($_SESSION['objetivo'] == "hipertrofia")
+    $ultima_ficha_completa = 6;
+
 $sql =
     "INSERT INTO 
-        usuarios (nome, email, cpf, nascimento, senha, plano, numero_cartao, titular_cartao, vencimento, cvv, cpf_titular, objetivo, _id_ultima_ficha) 
+        usuarios (nome, email, cpf, nascimento, senha, plano, numero_cartao, titular_cartao, vencimento, cvv, cpf_titular, objetivo, _id_ultima_ficha, ultima_ficha_completa) 
     VALUES 
         ('" . $_SESSION['nome'] . "', '" . $_SESSION['email'] . "', '" . $_SESSION['cpf'] . "', '" . $_SESSION['nascimento'] . "', MD5('" . $_SESSION['senha'] . "'),
          '" . $_SESSION['plano'] . "', '" . $_SESSION['numero_cartao'] . "', '" . $_SESSION['titular'] . "', '" . $_SESSION['vencimento'] . "', '" . $_SESSION['cvv'] . "',
-          '" . $_SESSION['cpf_titular'] . "',  '" . $_SESSION['objetivo'] . "', '" . $_id_ultima_ficha . "')";
+          '" . $_SESSION['cpf_titular'] . "',  '" . $_SESSION['objetivo'] . "', '" . $_id_ultima_ficha . "', '" . $ultima_ficha_completa . "')";
 
 if ($mysqli->query($sql) === true)
     header('Location: ../home.php');
