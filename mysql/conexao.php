33952fe280c00bb4e1ce3754a27de033a0d26f65
Cristian <crisvini.leoncini@outlook.com>
2022-08-11 21:58:27 -0300

    alteração credenciais do banco

--- mysql/conexao.php
+++ mysql/conexao.php
@@ -1,5 +1,11 @@
 <?php
 
-$mysqli = new mysqli('localhost', 'root', '', 'workout');
+$server = "workoutmysql.ch8ztzhdytvl.us-east-1.rds.amazonaws.com";
+$user = "admin";
+$password = "workoutmysql";
+$database = "workoutmysql";
+$port = 3306;
+
+$mysqli = new mysqli($server, $user, $password, $database, $port);
 if ($mysqli->connect_errno)
     echo "Falha ao conectar: (" . $mysqli->connect_errno . ") " .  $mysqli->connect_error;
